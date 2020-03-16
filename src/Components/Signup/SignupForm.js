bb6e4aa5fcfad5816f9741708bc4e600ede6cc45
Lucas Olivera <oliveralucasm@gmail.com>
2020-03-16 11:15:34 -0300

    Handle the control of the signup form to the Formik library

--- src/Components/Signup/SignupForm.js
+++ src/Components/Signup/SignupForm.js
@@ -1,28 +1,41 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { useFormik } from 'formik'
 
 export default function SignupForm () {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: ''
+    },
+    onSubmit: values => {
+      console.log(values)
+    }
+  })
+
   return (
     <Form>
       <Form.Group controlId='firstName'>
-        <Form.Control placeholder='First name' />
+        <Form.Control placeholder='First name' onChange={formik.handleChange} />
       </Form.Group>
 
       <Form.Group controlId='lastName'>
-        <Form.Control placeholder='Last name' />
+        <Form.Control placeholder='Last name' onChange={formik.handleChange} />
       </Form.Group>
 
       <Form.Group controlId='username'>
-        <Form.Control placeholder='Username' />
+        <Form.Control placeholder='Username' onChange={formik.handleChange} />
       </Form.Group>
 
       <Form.Group controlId='password'>
-        <Form.Control type='password' placeholder='Password' />
+        <Form.Control type='password' placeholder='Password' onChange={formik.handleChange} />
       </Form.Group>
 
       <Form.Group controlId='signupButton'>
-        <Button variant='primary' type='submit' block>
+        <Button variant='primary' type='submit' block onClick={formik.handleSubmit}>
           Sign up
         </Button>
       </Form.Group>
