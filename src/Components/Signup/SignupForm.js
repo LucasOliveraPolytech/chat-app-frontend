e01a98ddaf57eccbade044d437fd6ee5d99d55f7
Lucas Olivera <oliveralucasm@gmail.com>
2020-03-18 17:57:09 -0300

    Connect the signup page to its mutation

--- src/Components/Signup/SignupForm.js
+++ src/Components/Signup/SignupForm.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { Alert } from 'react-bootstrap'
 import { useFormik } from 'formik'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks'
+
+const SIGNUP_NEW_USER = gql`
+  mutation signupNewUser($signupInput: SignupInput!) {
+    signup(userData: $signupInput) {
+      user {
+        id
+      }
+      jwt
+    }
+  }
+`
 
 export default function SignupForm () {
+  const [error, setError] = useState()
+  const [signup, { data }] = useMutation(SIGNUP_NEW_USER)
   const formik = useFormik({
     initialValues: {
       firstName: '',
@@ -11,13 +27,21 @@ export default function SignupForm () {
       username: '',
       password: ''
     },
-    onSubmit: values => {
-      console.log(values)
+    onSubmit: async values => {
+      try {
+        const signupInput = { signupInput: values }
+        await signup({ variables: signupInput })
+      } catch (error) {
+        setError(error)
+      }
     }
   })
 
   return (
     <Form>
+      <Alert variant='danger' show={Boolean(error)}>
+        Oh Snap! Something went wrong.
+      </Alert>
       <Form.Group controlId='firstName'>
         <Form.Control placeholder='First name' onChange={formik.handleChange} />
       </Form.Group>
