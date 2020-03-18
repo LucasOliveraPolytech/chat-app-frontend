6425cfb791e02aa8a21122a28df884ebf3f344a4
Lucas Olivera <oliveralucasm@gmail.com>
2020-03-18 18:44:20 -0300

    Connect the signin page to its mutation

--- src/Components/Signin/SigninForm.js
+++ src/Components/Signin/SigninForm.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+import { useFormik } from 'formik'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks'
+
+const SIGNIN_USER = gql`
+  mutation signinUser($signinInput: SigninInput!) {
+    signin(userData: $signinInput) {
+      user {
+        id
+      }
+      jwt
+    }
+  }
+`
 
 export default function SigninForm () {
+  const [error, setState] = useState()
+  const [signin, { data }] = useMutation(SIGNIN_USER)
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: async values => {
+      try {
+        const signinInput = { signinInput: values }
+        await signin({ variables: signinInput })
+      } catch (error) {
+        setState(error)
+      }
+    }
+  })
+
   return (
     <Form>
+      <Alert variant='danger' show={Boolean(error)}>
+        Oh Snap! Something went wrong.
+      </Alert>
       <Form.Group controlId='username'>
-        <Form.Control placeholder='Username' />
+        <Form.Control placeholder='Username' onChange={formik.handleChange} />
       </Form.Group>
 
       <Form.Group controlId='password'>
-        <Form.Control type='password' placeholder='Password' />
+        <Form.Control type='password' placeholder='Password' onChange={formik.handleChange} />
       </Form.Group>
 
       <Form.Group controlId='signinButton'>
-        <Button variant='primary' type='submit' block>
+        <Button variant='primary' type='submit' block onClick={formik.handleSubmit}>
           Sign in
         </Button>
       </Form.Group>
